4b62e5b518c1c2bdf4754af0d43d56480a1427e5
Istam <mamadaliyevistam@gmail.com>
2025-11-06 13:39:37 +0500

    Add Parrot program to read and print three phrases in sequence

---
+++ tasks/1_Linear_programs/Group_Work/5_Parrot.py
@@ -0,0 +1,21 @@
+#-----Parrot------
+
+#-----Write a program that first reads three phrases in sequence, and then plays them back in the same sequence, each on a separate line.
+
+
+#===========================================================================================
+#==============SOLUTION=============
+
+a = input("")
+b = input("")
+c = input("")
+print(a + "\n" + b + "\n" + c)
+
+
+
+
+
+
+
+
+
