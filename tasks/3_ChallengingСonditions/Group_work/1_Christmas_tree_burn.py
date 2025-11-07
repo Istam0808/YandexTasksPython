c59631243ccbcd759ea30508273183d1b1cedf87
Istam <mamadaliyevistam@gmail.com>
2025-11-07 22:37:25 +0500

    task

---
+++ tasks/3_ChallengingСonditions/Group_work/1_Christmas_tree_burn.py
@@ -0,0 +1,19 @@
+#-----Chrismas tree burn------
+
+#-----Write a program that reads three strings. If the three strings are "one," "two," and "three," the program prints "BURN." If not, it prints "DON'T BURN."
+
+
+
+
+
+#===========================================================================================
+#==============SOLUTION=============
+
+first = input()
+second = input()
+third = input()
+
+if first == "one" and second == "two" and third == "three":
+    print("BURN")
+else:
+    print("DON'T BURN")
