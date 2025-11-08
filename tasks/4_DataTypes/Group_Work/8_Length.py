5b878520c42bb0f06b1b260a42abdeac9c7543cb
Istam <mamadaliyevistam@gmail.com>
2025-11-08 14:51:00 +0500

    one more task

---
+++ tasks/4_DataTypes/Group_Work/8_Length.py
@@ -0,0 +1,11 @@
+# Write a program that reads a string from the keyboard and prints the phrase:
+
+# "The word [input string] has length [length of input string]."
+
+
+#===========================================================================================
+#==============SOLUTION=============
+
+word = input("")
+len_ = len(word)
+print(f"Слово {word} имеет длину {len_}")
