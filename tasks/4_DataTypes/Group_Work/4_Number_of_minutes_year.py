2623d9d009a65d66f192eb9968ec4cf738bc28d6
Istam <mamadaliyevistam@gmail.com>
2025-11-08 14:40:31 +0500

    Add new task to calculate the number of minutes in a non-leap year and update Alisa and time task with solution comments

---
+++ tasks/4_DataTypes/Group_Work/4_Number_of_minutes_year.py
@@ -0,0 +1,19 @@
+# days_per_year = 365
+# hours_per_day = 24
+# minutes_per_hour = 60
+
+# Допишите программу так, чтобы она выводила количество минут в (не високосном) году, используя эти данные.
+
+# Формат вывода
+# Выводится одно число.
+
+
+
+#===========================================================================================
+#==============SOLUTION=============
+days_per_year = 365
+hours_per_day = 24
+minutes_per_hour = 60
+
+minutes_in_year = days_per_year * hours_per_day * minutes_per_hour
+print(minutes_in_year)
