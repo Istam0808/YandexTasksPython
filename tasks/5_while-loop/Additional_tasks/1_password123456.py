d65b949b1c05476dc206b239e263b619eb455320
Istam0808 <mamadaliyevistam@gmail.com>
2025-11-20 22:28:18 +0500

    commit

---
+++ tasks/5_while-loop/Additional_tasks/1_password123456.py
@@ -0,0 +1,14 @@
+password1 = input()
+password2 = input()
+
+if len(password1) < 8:
+    print("Короткий!")
+elif "123" in password1:
+    print("Простой!")
+elif password1 != password2:
+    print("Различаются.")
+else:
+    print("OK")
+
+
+
